37d7513e37e8f7df3b854c35cbce75bb67682f59
Luca Orio <luca.o@me.com>
2017-09-21 22:54:12 -0700

    Update dialog description

--- src/resize.js
+++ src/resize.js
@@ -16,7 +16,7 @@ const resize = ctx => {
 const getDimensions = () => {
   const alert = COSAlertWindow.new();
   alert.setMessageText('Resize Element(s)');
-  alert.setInformativeText('Enter new width / height. To skip a dimension leave its field empty. Resize is unconstrained.');
+  alert.setInformativeText('Enter new width / height. Leave an empty field to skip the dimension. Resize is unconstrained.');
 
   alert.addTextLabelWithValue('Width');
   alert.addTextFieldWithValue('');
